1c003a076d992995f8a20088a66916b4ac8f82c6
‘Di’ <Larra12@ya.ru>
2024-09-26 09:19:01 +0300

    app user create

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # Мои приложения
     'learning_logs',
+    'users'
     #  Встроенные приложения
     'django.contrib.admin',
     'django.contrib.auth',
